4ac30a02617e8f530983467d5d54cada5575349e
goce-arsoski <goce_ars@yahoo.com>
2020-05-14 17:39:11 +0200

    Add controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  # get 'tweets/index'
+  get 'tweets', to: 'tweets#index'
+
+  root 'tweets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
