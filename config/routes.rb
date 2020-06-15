54e540d1c52e35b03cb1ab7f50025b1fa0e45f59
goce-arsoski <goce_ars@yahoo.com>
2020-06-16 01:42:32 +0200

    CSS, HTML & JS update

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :tweets do
     resources :comments
   end
-  
+
   get 'tweets/all/:id', to: 'tweets#all', as: :tweets_all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
