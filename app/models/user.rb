b678f9e21fe80e91789f60f665ebf175ead682e5
goce-arsoski <goce_ars@yahoo.com>
2020-06-22 00:16:29 +0200

    Create User controller with methods new and create

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, length: { maximum: 50 },
-                    format: { wirh: VALID_EMAIL_REGEX },
+                    format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   root 'tweets#index'
 
   resources :tweets do
     resources :comments
   end
 
+  resources :users
   get 'tweets/all/:id', to: 'tweets#all', as: :tweets_all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
