bc4b9924e013daccc021b8c9fd174b69337fb948
goce-arsoski <goce_ars@yahoo.com>
2020-06-21 23:31:40 +0200

    Add column password to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_204329) do
+ActiveRecord::Schema.define(version: 2020_06_21_211257) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_06_21_204329) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
