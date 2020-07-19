99d9e738cf9612843e7978c25b8e697dcf24de10
goce-arsoski <goce_ars@yahoo.com>
2020-07-19 15:09:30 +0200

    Remove User from Tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,6 +3,5 @@ class Tweet < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :user
 
-  validates :user, presence: true, length: { minimum: 2 }
   validates :content, presence: true, length: { maximum: MAX_CONTENT_LENGTH }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_124754) do
+ActiveRecord::Schema.define(version: 2020_07_19_130115) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2020_07_11_124754) do
   end
 
   create_table "tweets", force: :cascade do |t|
-    t.string "user"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
